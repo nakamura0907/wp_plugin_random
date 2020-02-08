0ba6c024e48d22006ac3570d5ccdd7837258e675
ampan0907 <1801010814fx@cyber-u.ac.jp>
2020-02-08 10:36:59 +0900

    ランダムな記事が簡単に取得できるようになった。ただしループ部分が冗長。 次回は非推奨らしいquery_postsの変更と、コンストラクタに指定しているループ回数の動的な指定

--- Random.php
+++ Random.php
@@ -8,7 +8,7 @@ Version: 0.1
 Author URI:
 */
 
-$random = new Random(2);
+$random = new Random(5);
 
 Class Random
 {
@@ -26,19 +26,33 @@ Class Random
 
     private function display()
     {
+        query_posts(array('orderby' => 'rand', 'showposts' => $this->num)); //非推奨らしいので代替案を探す
         if (have_posts()) {
-            $i = 0;
-            while ($i <= $this->nun + 1) {
-                $this->loop();
+            while (have_posts()) {
                 the_post();
-                $i++;
+                $this->loop();
             }
+            wp_reset_query();
         }
+
+    }
+
+    private function getLoop()
+    {
+        global $wp_query;
+        return $wp_query->current_post+1;
     }
 
     private function loop()
     {
-        echo '<article>test</article>';
+        echo '<article class=article-content><a href=' . get_permalink($post->ID) . '>';
+        if (has_post_thumbnail()) {
+            the_post_thumbnail('medium');
+        } else {
+            echo '<img src=' . get_template_directory_uri() . '/assets/img/no_image.png alt=no_image>';
+        }
+        echo '<h2>' . get_the_title() . '</h2></a></article>' ;
     }
+
 
 }
